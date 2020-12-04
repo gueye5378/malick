c14770c6abb55bec0196beda02b529583e034b2f
gueye <gueyemalick125@gmail.com>
2020-12-04 13:16:12 +0100

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,42 @@
+const sr = ScrollReveal();
+sr.reveal('.header_img' , {
+    duration: 1500,
+    origin : 'left',
+    reset : true,
+    distance: '300px'
+});
+
+sr.reveal('.header_button' , {
+    duration: 1500,
+    origin: 'left',
+    reset: true,
+    distance: '300px'
+});
+sr.reveal('.subtext_services' , {
+    duration: 1000,
+    origin: 'left',
+    reset: true,
+    distance: '100px'
+});
+
+sr.reveal('.service_title' , {
+    duration: 1000,
+    origin: 'bottom',
+    reset: true,
+    distance: '300px',
+    delay : 200
+});
+sr.reveal('.title_' , {
+    duration: 1000,
+    origin: 'left',
+    reset: true,
+    distance: '200px',
+    delay : 200
+});
+sr.reveal('.title_projet' , {
+    duration: 1000,
+    origin: 'top',
+    reset: true,
+    distance: '200px',
+    delay : 200
+});
